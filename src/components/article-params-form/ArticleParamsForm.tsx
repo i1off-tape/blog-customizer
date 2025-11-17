2faf2250fd143437141eb7294e1b7f7b89d744b9
I1OFF <jhoncrendy18@gmail.com>
2025-11-17 03:18:11 +0300

    feat: add base logic render form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,14 +1,67 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { Select } from 'src/ui/select';
+import {
+	ArticleStateType,
+	OptionType,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
+import { useState, useRef } from 'react';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { Text } from 'src/ui/text';
+import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
 
-export const ArticleParamsForm = () => {
+type ArticleParamsFormProps = {
+	currentArticleState: ArticleStateType;
+	setCurrentArticleState: (param: ArticleStateType) => void;
+};
+
+export const ArticleParamsForm = ({
+	currentArticleState,
+	setCurrentArticleState,
+}: ArticleParamsFormProps) => {
+	const [isOpen, setIsOpen] = useState<boolean>(false);
+	const rootRef = useRef<HTMLDivElement | null>(null);
+	const [selectArticleState, setSelectArticleState] =
+		useState<ArticleStateType>(currentArticleState);
+
+	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
+		setSelectArticleState({ ...selectArticleState, [key]: value });
+	};
+
+	useOutsideClickClose({
+		isOpen,
+		rootRef,
+		onClose: () => setIsOpen(false),
+		onChange: setIsOpen,
+	});
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<ArrowButton
+				isOpen={isOpen}
+				onClick={() => {
+					setIsOpen(!isOpen);
+				}}
+			/>
+			<aside
+				className={clsx(styles.container, isOpen && styles.container_open)}
+				ref={rootRef}>
+				<form
+					className={styles.form}
+					onSubmit={(e) => {
+						e.preventDefault();
+						setCurrentArticleState(selectArticleState);
+					}}>
+					<Text size={38}>Задайте параметры</Text>
+					<Select
+						selected={selectArticleState.fontFamilyOption}
+						title='Шрифт'
+						options={fontFamilyOptions}
+						onChange={(option) => handleChange('fontFamilyOption', option)}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
