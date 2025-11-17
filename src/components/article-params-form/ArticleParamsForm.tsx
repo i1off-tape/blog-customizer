27ad9d83c4a64842c27648756e1954537ef49b20
I1OFF <jhoncrendy18@gmail.com>
2025-11-17 03:53:10 +0300

    feat(articleparamsform): add all functions for form, but without verstka form

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -4,7 +4,12 @@ import { Select } from 'src/ui/select';
 import {
 	ArticleStateType,
 	OptionType,
+	backgroundColors,
+	contentWidthArr,
+	fontColors,
 	fontFamilyOptions,
+	fontSizeOptions,
+	defaultArticleState,
 } from 'src/constants/articleProps';
 import { useState, useRef } from 'react';
 import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
@@ -12,6 +17,8 @@ import { Text } from 'src/ui/text';
 import clsx from 'clsx';
 
 import styles from './ArticleParamsForm.module.scss';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
 
 type ArticleParamsFormProps = {
 	currentArticleState: ArticleStateType;
@@ -29,6 +36,11 @@ export const ArticleParamsForm = ({
 
 	const handleChange = (key: keyof ArticleStateType, value: OptionType) => {
 		setSelectArticleState({ ...selectArticleState, [key]: value });
+	};
+
+	const handleReset = () => {
+		setSelectArticleState(defaultArticleState);
+		setCurrentArticleState(defaultArticleState);
 	};
 
 	useOutsideClickClose({
@@ -62,8 +74,41 @@ export const ArticleParamsForm = ({
 						options={fontFamilyOptions}
 						onChange={(option) => handleChange('fontFamilyOption', option)}
 					/>
+					<RadioGroup
+						title='размер шрифта'
+						name='fontSize'
+						options={fontSizeOptions}
+						selected={selectArticleState.fontSizeOption}
+						onChange={(option) =>
+							handleChange('fontSizeOption', option)
+						}></RadioGroup>
+					<Select
+						selected={selectArticleState.fontColor}
+						title='цвет шрифта'
+						options={fontColors}
+						onChange={(option) => handleChange('fontColor', option)}></Select>
+					<Separator />
+					<Select
+						selected={selectArticleState.backgroundColor}
+						title='цвет фона'
+						options={backgroundColors}
+						onChange={(option) =>
+							handleChange('backgroundColor', option)
+						}></Select>
+					<Select
+						selected={selectArticleState.contentWidth}
+						title='ширина контента'
+						options={contentWidthArr}
+						onChange={(option) =>
+							handleChange('contentWidth', option)
+						}></Select>
 					<div className={styles.bottomContainer}>
-						<Button title='Сбросить' htmlType='reset' type='clear' />
+						<Button
+							title='Сбросить'
+							htmlType='reset'
+							type='clear'
+							onClick={handleReset}
+						/>
 						<Button title='Применить' htmlType='submit' type='apply' />
 					</div>
 				</form>
